ec9cd06a8046d3e968a0b5c03de71db2ca1d6d17
holufred <atoyebifrederick@gmail.com>
2024-06-20 13:12:11 -0700

    Q4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/*  demonstrating the function of puts */
-int main(void) /* the function */
+/** 
+ *  main - Demonst
+ */
+int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
