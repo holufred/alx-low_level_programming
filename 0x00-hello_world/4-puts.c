24a171262a22a0c3ffd137449c55b8ed3e167c8a
holufred <atoyebifrederick@gmail.com>
2024-06-20 12:55:16 -0700

    Q$

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* demonstrating the function of puts */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return 0;
 }
