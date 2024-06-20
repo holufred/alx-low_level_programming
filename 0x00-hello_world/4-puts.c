882fd58c14daf5954641f2cf7593897e0e42f6dc
holufred <atoyebifrederick@gmail.com>
2024-06-20 13:18:46 -0700

    Q4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /**
  * main - Prints '"Programming is like building a multilingual puzzle"'
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0);/* returns 0 */
 }
