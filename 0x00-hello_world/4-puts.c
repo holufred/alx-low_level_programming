99557646872d827433b845a0c6954dffb793d19d
holufred <atoyebifrederick@gmail.com>
2024-06-20 13:04:01 -0700

    Q4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-/**
- *  demonstrating the function of puts
- */
-int main(void)
+/*  demonstrating the function of puts */
+int main()
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
