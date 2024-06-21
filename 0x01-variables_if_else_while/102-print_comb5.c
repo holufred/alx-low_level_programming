2a82ecf418170be182c5ffa7483ac81f32d7f0e9
holufred <atoyebifrederick@gmail.com>
2024-06-21 16:29:40 -0700

    Q12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
  * main - Prints all possible combinations of two two-digit numbers.
- *
  * Returns: Always returns 0 (Success)
  */
 int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = num1; num2 <= 99; num2++)
+		for (num2 = 0; num2 <= 99; num2++)
 		{
 			putchar('0' + num1 / 10);
 			putchar('0' + num1 % 10);
