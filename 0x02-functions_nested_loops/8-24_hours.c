9e3c1e17ff38b685d2af58a67a37d11038125dda
holufred <atoyebifrederick@gmail.com>
2024-06-25 14:57:17 -0700

    Q8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,12 +10,12 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
-			_putchar('0' + hour / 10);
-		       	_putchar('0' + hour % 10);
+			_putchar('0' + hour/10);
+			_putchar('0' + hour % 10);
 			_putchar(':');
 			_putchar('0' + minute / 10);
 			_putchar('0' + minute % 10);
-		       	_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
