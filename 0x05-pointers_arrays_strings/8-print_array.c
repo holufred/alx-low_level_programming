03cb41fe16c619b72c2bbd2e8e3d0961325b7092
holufred <atoyebifrederick@gmail.com>
2024-07-01 15:32:49 -0700

    Q8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an array of integers.
+ * @a: Pointer to the array of integers.
+ * @n: Number of elements of the array to be printed.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+			printf("%d, ", a[i]);
+		else
+		       	printf("%d", a[i]);
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,6 +10,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 int _putchar(char c);
+void print_array(int *a, int n);
 void puts_half(char *str);
 
 #endif /* MAIN_H */
