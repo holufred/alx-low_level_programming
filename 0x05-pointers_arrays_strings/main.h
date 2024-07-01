ca18dbbf0b5f6e71f9f09fd5a1128c50b442e70d
holufred <atoyebifrederick@gmail.com>
2024-07-01 15:05:33 -0700

    Q4

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-vpod print_rev(char *s);
+void print_rev(char *s);
 int _putchar(char c);
 
 #endif /* MAIN_H */
