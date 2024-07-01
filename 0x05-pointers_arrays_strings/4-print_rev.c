fabf6aef4d935c55bed0640035799f96982112de
holufred <atoyebifrederick@gmail.com>
2024-07-01 15:09:23 -0700

    Q4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,11 +5,14 @@
  */
 void print_rev(char *s)
 {
-	int len = 0;while (s[len] != '\0')
+	int len = 0;
+	int i;
+
+	while (s[len] != '\0')
 	{
 		len++;
 	}
-	for (int i = len - 1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
